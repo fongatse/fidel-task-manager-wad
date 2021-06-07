440d46a02dce5a134347469959aafda0f4c6e3f5
Fidel Ngatse <fngatse@sessionm.com>
2021-06-08 01:30:54 +0200

    add redux

---
+++ server/defaultState.js
@@ -0,0 +1,34 @@
+export const defaultState = {
+    users:[{
+        id:"u1",
+        name:"Dev"
+    },
+    {
+        id:"u2",
+        name:"Admin"
+    }],
+    groups:[{
+        name:"To Do",
+        id:"g1", 
+        owner:"u1"
+    },],
+    tasks:[{
+        name:"Do tests",
+        id:"t1",
+        group:"g1",
+        owner:"u1",
+        isComlete: false
+    },
+   { name:"Second task",
+    id:"t2",
+    group:"g1",
+    owner:"u2",
+    isComlete: true
+}],
+    comments:[{
+        owner:"u1",
+        id:"c1",
+        task:"t1",
+        content:"Super comment"
+    }]
+}

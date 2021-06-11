a6ca87978137def11df7a443917f388643542533
Fidel Ngatse <fngatse@sessionm.com>
2021-06-12 00:11:12 +0200

    fix db connection

--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -1,10 +1,11 @@
 import { MongoClient } from 'mongodb';
-const url = process.env.MONGODB_URI || `mongodb://localhost:27017/organizer`;
+const url = "mongodb+srv://admin:admin2@cluster0.5boba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 let db = null;
 
 export async function connectDB(){
     if (db) return db;
     let client = await MongoClient.connect(url, { useNewUrlParser: true });
     db = client.db();
+    console.log(db)
     return db;
 }
